c457b018423801ce8509da66bc35d6bed4860c41
waugust <waugustynski@gmail.com>
2010-02-23 07:47:34 -0800

    changing load precedence

--- public/javascripts/tufflife_base.js
+++ public/javascripts/tufflife_base.js
@@ -1,4 +1,12 @@
-dojo.registerModulePath("tufflife","../tufflife");
-dojo.require("tufflife.main");
-dojo.require("tufflife.data",true);
+dojo.addOnLoad(function(){
+    dojo.registerModulePath("tufflife","../tufflife");
+    dojo.require("tufflife.data");
+    dojo.require("tufflife.controllers.storage");
+    dojo.require("tufflife.controllers.character");
+    dojo.require("tufflife.controllers.equipment");
+    
+    dojo.addOnLoad(function(){
+        dojo.require("tufflife.main");
+        })
 
+})
