3e4a236226befd924c8cfb6dd76e58ea6e4c1024
waugust <waugustynski@gmail.com>
2010-02-28 10:25:12 -0800

    evnts.subscriber module, outlining subscribers

---
+++ public/javascripts/tufflife/events/subscribers.js
@@ -0,0 +1,8 @@
+dojo.provide("tufflife.events.subscribers")
+
+
+
+dojo.subscribe("itemEquip",tufflife.controllers.item,"equip");
+dojo.subscribe("itemUnequip",tufflife.controllers.item,"unequip");
+dojo.subscribe("itemDeposit",tufflife.controllers.item,"deposit");
+dojo.subscribe("itemWithdrawl",tufflife.controllers.item,"withdrawl");
