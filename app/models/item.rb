b98cf24fb19093b995a70daa58321bf12f126613
waugust <waugustynski@gmail.com>
2010-02-28 10:03:38 -0800

    got rid of the option to equip items from the bank

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ActiveRecord::Base
   has_and_belongs_to_many :banks
 
    def ingest(char)
-    if self.kind == "Drug"
+    if self.type == "Drug"
       char.addstats(self)
       char.intoxicated=true
       char.intoxicated_at=Time.now
@@ -17,24 +17,14 @@ class Item < ActiveRecord::Base
   def deposit(char)
     if char.bank.room>0
       if char.items.include?(self)
-        if self.kind=="Inventory"
+        if self.type=="Inventory"
           char.bank.capacity+=self.capacity
         end
         char.inventory.items.delete(self)
         char.bank.items<<self
-      elsif char.eqitems.has_value?(self)
-        slot=char.eqitems.index(self)
-        char.equipment[slot]=0
-        char.bank.items<<self
-        char.equipment.items.delete(self)
-        char.bank.save
-        char.equipment.save
-        char.save
       else
         #TODO
       end
-      char.bank.items<<self
-      char.inventory.items.delete(self)
       char.save
     else
       #TODO
@@ -43,7 +33,7 @@ class Item < ActiveRecord::Base
   def withdrawl(char)
     if char.bank.items.include?(self)
       if char.inventory.room>0
-          if self.kind=="Inventory"
+          if self.type=="Inventory"
             char.bank.capacity-=self.capacity
           end
           char.inventory.items<<self
