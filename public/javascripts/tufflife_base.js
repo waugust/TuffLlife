08866edb91018f252c86cedd92f224d1cbf11360
waugust <waugustynski@gmail.com>
2010-02-28 10:11:08 -0800

    controllers.item, events.subscribers, events.errors, and controllers.time modules

--- public/javascripts/tufflife_base.js
+++ public/javascripts/tufflife_base.js
@@ -4,9 +4,13 @@ dojo.addOnLoad(function(){
     dojo.require("tufflife.controllers.storage");
     dojo.require("tufflife.controllers.character");
     dojo.require("tufflife.controllers.equipment");
+    dojo.require("tufflife.controllers.item")
+    dojo.require("tufflife.events.subscribers",true)
+    dojo.require("tufflife.events.errors");
     
     dojo.addOnLoad(function(){
         dojo.require("tufflife.main");
+        dojo.require('tufflife.controllers.time');
         })
 
 })
